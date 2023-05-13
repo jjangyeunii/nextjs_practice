6999d2b5f02626cdfae505cd3bd143717338a9d6
jjangyeunii <euneun0927@naver.com>
2023-05-13 15:13:22 +0900

    fetch 사용: CSR test

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,6 @@
 import { getProducts } from "@/service/products";
 import Link from "next/link";
-import styles from "./page.module.css";
+import MeowArticle from "@/components/MeowArticle";
 
 // ISR 구현 방법
 // export const revalidate = 3;
@@ -10,12 +10,6 @@ export default async function ProductsPage() {
   // revalidate: 0이면 SSR 구현 가능
   // cache -> force-cache는 SSG로 작동, no-store는 SSR로 작동
   const products = await getProducts();
-  const res = await fetch("https://meowfacts.herokuapp.com", {
-    // next: { revalidate: 0 },
-    cache: "no-store",
-  });
-  const data = await res.json();
-  const factText = data.data[0];
   return (
     <>
       <h1>제품 소개 페이지!</h1>
@@ -26,7 +20,7 @@ export default async function ProductsPage() {
           </li>
         ))}
       </ul>
-      <article className={styles.article}>{factText}</article>
+      <MeowArticle />
     </>
   );
 }
